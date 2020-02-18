c3734730f0afb2d3c61aa82a75a0aeac81c4166c
Peyman Beyranvand <peyman.beyranvand@arvatocrmturkey.com>
2020-02-18 18:26:03 +0300

    featMatching refactored

--- DCGAN/losses.py
+++ DCGAN/losses.py
@@ -1,7 +1,12 @@
 import torch
+from torch.nn import BCELoss, MSELoss
+
+# BCE loss function
+criterion = BCELoss()
+feature_matching_criterion = MSELoss()
 
 
-def halfBCE(output, label, criterion, device):
+def halfBCE(output, label, device):
 
     b_size = output.view(-1).size(0)
     target_label = torch.full((b_size,), label, device=device)
@@ -10,29 +15,37 @@ def halfBCE(output, label, criterion, device):
     return loss
 
 
-def D_loss(real_batch, fake_batch, disc, gen, real_label, fake_label, criterion, device):
+def D_loss(real_batch, fake_batch, disc, gen, real_label, fake_label, device, featMatch=False):
     # all-real batch
-    output_real, _, _ = disc(real_batch)
+    output_real, f1, f2 = disc(real_batch)
     errD_real = halfBCE(output_real,
                         real_label,
-                        criterion,
                         device)
 
     # all-fake batch
     output_fake, _, _ = disc(fake_batch.detach())
     errD_fake = halfBCE(output_fake,
                         fake_label,
-                        criterion,
                         device)
 
-    return errD_real + errD_fake, errD_real, output_fake
+    if featMatch:
+        return errD_real + errD_fake, errD_real, output_fake, f1.detach(), f2.detach()
+    else:
+        return errD_real + errD_fake, errD_real, output_fake
 
 
-def G_loss(fake_batch, disc, real_label, criterion, device):
+def G_loss(fake_batch, disc, real_label, device, featMatch=False):
     output, _, _ = disc(fake_batch)
     errG = halfBCE(output,
                    real_label,  # to train G we use real labels
-                   criterion,
                    device)
 
     return errG, output
+
+
+def G_featMatch_loss(fake_batch, disc, real_label, device, f1real, f2real):
+    output, f1, f2 = disc(fake_batch)
+    fm_loss1 = feature_matching_criterion(f1, f1real)
+    fm_loss2 = feature_matching_criterion(f2, f2real)
+
+    return fm_loss1 + fm_loss1, output
