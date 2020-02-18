63c1aca1b05efb937d13b55003cdf9e976b062af
Peyman Beyranvand <peyman.beyranvand@arvatocrmturkey.com>
2020-02-18 15:26:45 +0300

    D training refactored

---
+++ DCGAN/networks.py
@@ -0,0 +1,94 @@
+from torch import nn
+
+# the generator
+
+
+class Generator(nn.Module):
+    def __init__(self, ngpu, nz, ngf, nc):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu, nc, ndf):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.features1 = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+        )
+
+        self.features2 = nn.Sequential(
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+        )
+
+        self.classifier = nn.Sequential(
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        f1 = self.features1(input)
+        f2 = self.features2(f1)
+        cls = self.classifier(f2)
+        return cls, f1, f2
+
+
+def weights_init(m):
+    """
+    Custom weight initializer from the DCGAN papar
+    Different from the usual way of initializing the weigths
+
+    Params:
+    -----------------------------------
+    m:: network object
+
+    -----------------------------------
+    DCGAN paper arXiv:1511.06434
+    """
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
---
+++ DCGAN/utils.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.utils as vutils
+from torchvision.datasets import CIFAR10, MNIST
+from torch.utils.data import DataLoader
+from torch import nn
+
+
+def visual_data(batch, device):
+    """
+    Visualize a batch of image data
+
+    Params
+    --------------------------------
+    batch:: a batch of images to visualize
+            in numpy matrix format
+
+    device:: cpu/gpu device used for computations
+    """
+    plt.figure(figsize=(12, 12))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(batch[0].to(device)[:64],
+                                             padding=2, normalize=True).cpu(), (1, 2, 0)))
+
+
+def load_mnist(mnistFolder, tsfms, batchSize=64, numWorkers=0):
+    """
+    Load, download if necessary, the CIFAR10 data and
+    return the iterable batch generator object
+
+    Params
+    --------------------------------
+    cifarFolder:: folder to store/load the CIFAR10 data from
+    tsfms:: transformations requaired to apply on data
+    batchSize:: number of samples on each batch of the data
+    numWorkers:: number of cpu cores used to load the data
+
+    Return:
+    --------------------------------
+    trainLoader:: iterable training data batch generator object
+    testLoader:: iterable test data batch generator object
+    """
+    trainData = MNIST(mnistFolder, download=True, train=True, transform=tsfms)
+    trainLoader = DataLoader(
+        trainData, batch_size=batchSize, num_workers=numWorkers)
+
+    testData = MNIST(mnistFolder, download=True, train=False, transform=tsfms)
+    testLoader = DataLoader(
+        testData, batch_size=batchSize, num_workers=numWorkers)
+
+    return trainLoader, testLoader
+
+
+def load_cifar10(cifarFolder, tsfms, batchSize=64, numWorkers=0):
+    """
+    Load, download if necessary, the CIFAR10 data and
+    return the iterable batch generator object
+
+    Params
+    --------------------------------
+    cifarFolder:: folder to store/load the CIFAR10 data from
+    tsfms:: transformations requaired to apply on data
+    batchSize:: number of samples on each batch of the data
+    numWorkers:: number of cpu cores used to load the data
+
+    Return:
+    --------------------------------
+    trainLoader:: iterable training data batch generator object
+    testLoader:: iterable test data batch generator object
+    """
+    trainData = CIFAR10(cifarFolder, download=True,
+                        train=True, transform=tsfms)
+    trainLoader = DataLoader(
+        trainData, batch_size=batchSize, num_workers=numWorkers)
+
+    testData = CIFAR10(cifarFolder, download=True,
+                       train=False, transform=tsfms)
+    testLoader = DataLoader(
+        testData, batch_size=batchSize, num_workers=numWorkers)
+
+    return trainLoader, testLoader
