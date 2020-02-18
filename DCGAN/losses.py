e35ff0e95d4068660abc7f3ae6688dcc9005d263
Peyman Beyranvand <peyman.beyranvand@arvatocrmturkey.com>
2020-02-18 19:13:43 +0300

    bugfix featmatching, updated refactored code

--- DCGAN/losses.py
+++ DCGAN/losses.py
@@ -15,37 +15,32 @@ def halfBCE(output, label, device):
     return loss
 
 
-def D_loss(real_batch, fake_batch, disc, gen, real_label, fake_label, device, featMatch=False):
+def D_loss(output_real, output_fake, real_label, fake_label, device):
     # all-real batch
-    output_real, f1, f2 = disc(real_batch)
     errD_real = halfBCE(output_real,
                         real_label,
                         device)
 
     # all-fake batch
-    output_fake, _, _ = disc(fake_batch.detach())
     errD_fake = halfBCE(output_fake,
                         fake_label,
                         device)
 
-    if featMatch:
-        return errD_real + errD_fake, errD_real, output_fake, f1.detach(), f2.detach()
-    else:
-        return errD_real + errD_fake, errD_real, output_fake
+    return errD_real + errD_fake, errD_real, output_fake
 
 
-def G_loss(fake_batch, disc, real_label, device, featMatch=False):
-    output, _, _ = disc(fake_batch)
+def G_loss(output, real_label, device):
+
     errG = halfBCE(output,
                    real_label,  # to train G we use real labels
                    device)
 
-    return errG, output
+    return errG
 
 
-def G_featMatch_loss(fake_batch, disc, real_label, device, f1real, f2real):
-    output, f1, f2 = disc(fake_batch)
-    fm_loss1 = feature_matching_criterion(f1, f1real)
-    fm_loss2 = feature_matching_criterion(f2, f2real)
+def G_featMatch_loss(f1fake, f2fake, f1real, f2real):
 
-    return fm_loss1 + fm_loss1, output
+    fm_loss1 = feature_matching_criterion(f1fake, f1real)
+    fm_loss2 = feature_matching_criterion(f2fake, f2real)
+
+    return fm_loss1 + fm_loss2
